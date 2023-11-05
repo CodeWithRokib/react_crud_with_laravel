0a32c2d32dca45383fc411746f5c31078e120902
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2023-11-06 00:34:11 +0600

    crud category post worked

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -4,20 +4,71 @@ import axios from 'axios';
 const Category = () => {
 
     const [data, setData] = useState([]);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState(null);
+    
 
     useEffect(()=>{
         axios.get('http://localhost:8000/api/category')
         .then((response)=>{
             setData(response.data);
             // console.log(response)
-        }).catch((response)=>{
-            console.log(response);
+        }).catch((err)=>{
+            console.log(err);
         })
     },[])
 
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+      };
+    
+      const handleDescriptionChange = (event) => {
+        setDescription(event.target.value);
+      };
+    
+      const handleImageChange = (event) => {
+        setImage(event.target.files[0]);
+      };
+    
+      const handleSubmit = async () => {
+        if (!title || !description || !image) {
+          console.error('Please fill in all fields');
+          return;
+        }
+    
+        const formData = new FormData();
+        formData.append('title', title);
+        formData.append('description', description);
+        formData.append('image', image);
+    
+        const uploadURL = 'http://localhost:8000/api/category';
+    
+        await axios.post(uploadURL, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+        .then(response => {
+          console.log('Data uploaded successfully:', response.data);
+          // Handle the response after successful upload
+        })
+        .catch(error => {
+          console.error('Error uploading data:', error);
+          // Handle errors if the upload fails
+        });
+      };
+    
+    
+ 
+
   return (
     
-        <div>
+        <div className='container'>
+                <input type="text" placeholder="Title" value={title} onChange={handleTitleChange} />
+                <textarea placeholder="Description" value={description} onChange={handleDescriptionChange} />
+                <input type="file" onChange={handleImageChange} />
+                <button onClick={handleSubmit}>Submit</button>
             <table className='table table-bordered'>
                 <thead>
                     <tr>
